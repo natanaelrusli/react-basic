66d15a891d44f9519d5ee2e4dc2249bd2ff68ada
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-09-21 11:44:05 +0700

    Add: seperate expense date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
+import ExpenseDate from './ExpenseDate'
+
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  const expenseDate = props.date
   const expenseTitle = props.title
   const expenseAmount = props.amount
 
   return (
     <div className='expense-item'>
-      <div>
-        { expenseDate.toISOString() }
-      </div>
+      <ExpenseDate date = {props.date} />
       <h2 className='expense-item__date'>
         { expenseTitle }
       </h2>
