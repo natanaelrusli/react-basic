8163bdcb563e779067d633057e513807f76c744f
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-09-21 14:39:30 +0700

    ref: component file organized to category

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses.js";
 
 function App() {
   // here is how you add a paragraph into a div using vanilla JS
