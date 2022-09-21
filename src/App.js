b242025802548e05817d834f53c7a0e8f35aabc4
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-09-21 14:34:54 +0700

    Add: Example element using createElement method

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -32,8 +33,16 @@ function App() {
     },
   ];
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // )
+
   return (
     <div>
+      <h2>Let's get started!</h2>
       <Expenses items = {expenses} />
     </div>
   );
